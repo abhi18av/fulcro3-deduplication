d53f6ab7377fbaf7f26923c8cf2e25d39b76fd26
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-08 21:33:39 +0530

    going beyond the basic mvp now

--- src/fulcro3_deduplication/core.clj
+++ src/fulcro3_deduplication/core.clj
@@ -1,15 +1,16 @@
-(ns fulcro3-deduplication.core
+(ns fulcro-deduplication.core
   (:import [org.apache.commons.text.similarity JaroWinklerDistance])
   (:require [clojure.java.io :as io]
             [multigrep.core  :as mgrep]))
 
 ;;;;;;;;
-(def fulcro3-src-file-paths
-  (let [fulcro3-src "./src/fulcro"
+
+(def fulcro-src-paths
+  (let [fulcro-src "./src/fulcro"
         file-extension-matcher (.getPathMatcher
                                 (java.nio.file.FileSystems/getDefault)
                                 "glob:*.{clj,cljs,cljc}")]
-    (->> fulcro3-src
+    (->> fulcro-src
          io/file
          file-seq
          (filter #(.isFile %))
@@ -17,23 +18,55 @@
          (mapv #(.getAbsolutePath %)))))
 
 (comment
-  (count fulcro3-src-file-paths)
-  (first fulcro3-src-file-paths))
+  (first fulcro-src-paths)
+  (count fulcro-src-paths))
 
 ;;;;;;;;
 
-(def all-defs
-  (mgrep/grep [#"defn"] (io/resource "example.clj")))
+(defn get-file-class-path [a-file]
+  (str "."
+       (second
+        (clojure.string/split
+         (first
+          (clojure.string/split a-file #":")) #"src"))))
+
+(comment
+  (get-file-class-path (nth fulcro-src-paths 1)))
+
+
+;;;;;;;;
+
+
+(def fulcro-class-paths
+  (map get-file-class-path fulcro-src-paths))
+
+(comment
+  (first fulcro-class-paths))
+
+;;;;;;;;
+
+(defn defs-in-a-file [a-file]
+  (mgrep/grep [#"defn"] (io/resource a-file)))
+
+(defs-in-a-file (first fulcro-class-paths))
 
 (comment
   (mgrep/grep [#"def " #"defn " #">defn " #"gw/>defn "] (io/resource "./fulcro/algorithms/merge.cljc"))
   (mgrep/grep [#"def"] (io/resource "./fulcro/algorithms/data_targeting.cljc"))
   (mgrep/grep #"def" (io/resource "./fulcro/algorithms/merge.cljc")))
 
+;;;;;;;;
+
+(def defs-in-all-files
+  (flatten
+   (map defs-in-a-file fulcro-class-paths)))
+
+(comment
+  (count defs-in-all-files)
+  (first defs-in-all-files))
 
 ;;;;;;;;
 ;; DONE drop the hashmaps which are not function defs
-
 
 (defn is-function-def? [a-hashmap]
   (let [result (some #(re-matches #"\(defn||\(defn-||\(>defn||\(gw\/>defn" %)
@@ -55,9 +88,10 @@
 
 (def all-defs-final
   (filter (fn [a-hashmap]
-            (is-function-def? a-hashmap)) all-defs))
+            (is-function-def? a-hashmap)) defs-in-all-files))
 
 (comment
+  (count all-defs-final)
   (first all-defs-final)
   (type
    (:file (first all-defs-final)))
@@ -78,15 +112,19 @@
   (get-file-name (first all-defs-final)))
 
 ;;;;;;;
+;; NOTE need to normalize for < */?/! >
 
 (defn get-normalized-function-name [a-java-net-url-object]
   (first
    (clojure.string/split
     (second
-     (clojure.string/split (:line a-java-net-url-object) #" ")) #"\*")))
+     (clojure.string/split (:line a-java-net-url-object) #" ")) #"\*|!|\?")))
 
 (comment
-  (get-normalized-function-name (first all-defs-final)))
+  (get-normalized-function-name (first all-defs-final))
+  (clojure.string/split "abcd*"  #"\*|!|\?")
+  (clojure.string/split "abcd!"  #"\*|!|\?")
+  (clojure.string/split "abcd?"  #"\*|!|\?"))
 
 ;;;;;;;
 
@@ -97,23 +135,24 @@
       false)))
 
 (comment
+  (are-similar-strings? "integrate-ident" "integrate-ident?")
+  (are-similar-strings? "integrate-ident" "integrate-ident!")
   (are-similar-strings? "integrate-ident" "integrate-ident*"))
 
 ;;;;;;;
-(def all-function-names
-  (map get-normalized-function-name all-defs-final))
 
 (def all-unique-function-names
-  (apply hash-set all-function-names))
+  (apply hash-set (map get-normalized-function-name all-defs-final)))
 
 ;;;;;;;
 
 ;; TODO filter the possible duplicates
 
 (doseq [keyval all-defs-final]
-  (println (:line keyval)))
+  (println keyval)
+  #_(println (:line keyval)))
 
+(comment
+  (zipmap [:a :b :c] (repeat 3 nil)))
 
-
-
-
+(zipmap (map keyword all-unique-function-names) (repeat (count all-unique-function-names) nil))
