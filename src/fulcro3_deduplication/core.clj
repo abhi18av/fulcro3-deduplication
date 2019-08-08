a1c9bd5bf04d365f5dcb1aa37140a3e5792db04c
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-08 17:01:08 +0530

    we can now filter the function-defs from multigrep results

--- src/fulcro3_deduplication/core.clj
+++ src/fulcro3_deduplication/core.clj
@@ -22,10 +22,37 @@
 
 ;;;;;;;;
 
-(mgrep/grep [#"def " #"defn " #">defn " #"gw/>defn "] (io/resource "./fulcro/algorithms/merge.cljc"))
+(def all-defs
+  (mgrep/grep [#"defn"] (io/resource "example.clj")))
 
 (comment
+  (mgrep/grep [#"def " #"defn " #">defn " #"gw/>defn "] (io/resource "./fulcro/algorithms/merge.cljc"))
+  (mgrep/grep [#"def"] (io/resource "./fulcro/algorithms/data_targeting.cljc"))
   (mgrep/grep #"def" (io/resource "./fulcro/algorithms/merge.cljc")))
 
 
 ;;;;;;;;
+
+;; DONE drop the hashmaps which are not function defs
+
+(defn is-function-def? [a-hashmap]
+  (let [result (some #(re-matches #"\(defn||\(defn-||\(>defn||\(gw\/>defn" %)
+                     (take 2
+                           (clojure.string/split (:line a-hashmap) #" ")))]
+    (if (= "" result)
+      false
+      true)))
+
+(comment
+  (is-function-def? (nth all-defs 3))
+  (map is-function-def? all-defs)
+  (some #(re-matches #"\(gw\/>defn" %) ["(gw/>defn"])
+  (some #(re-matches #"\(defn|\(defn-" %) '("(defn" "(defn-" "(>defn")))
+
+
+;;;;;;;;
+
+(def all-defs-final
+  (filter (fn [a-hashmap]
+            (is-function-def? a-hashmap)) all-defs))
+
